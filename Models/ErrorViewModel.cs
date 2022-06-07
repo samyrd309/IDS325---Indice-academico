7ef6f549db8a4448056e58f1b7ff244613620ff9
samyrd309 <samyrd309@gmail.com>
2022-06-07 19:22:19 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IDS325___Indice_academico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
