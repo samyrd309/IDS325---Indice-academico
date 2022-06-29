bec8b1cd018437667510002c544f44f3f3197fd8
samyrd309 <samyrd309@gmail.com>
2022-06-29 11:57:50 -0400

    .

--- Program.cs
+++ Program.cs
@@ -35,5 +35,5 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Publicacion}/{action=Index}/{id?}");
 app.Run();
